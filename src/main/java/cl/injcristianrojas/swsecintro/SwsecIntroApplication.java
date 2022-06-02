064439b325eab09b26c90e12e3ed322f2270301a
Cristian Rojas <injcristianrojas@gmail.com>
2022-06-02 18:02:05 -0400

    First commit

---
+++ src/main/java/cl/injcristianrojas/swsecintro/SwsecIntroApplication.java
@@ -0,0 +1,13 @@
+package cl.injcristianrojas.swsecintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwsecIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwsecIntroApplication.class, args);
+	}
+
+}
