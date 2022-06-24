ab26af65302fb4c38e57dc8561943600c2ff3cfc
Cristian Rojas <injcristianrojas@gmail.com>
2022-06-24 17:55:28 -0400

    Messages work

--- spa/src/app/api.service.ts
+++ spa/src/app/api.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private auth: AuthService) { }
+
+  getHeaders() {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.auth.getToken()
+      })
+    };
+  }
 
   login(username, password) {
     return this.http.post(
@@ -17,7 +27,10 @@ export class ApiService {
   }
   
   getPosts() {
-    return this.http.get('http://127.0.0.1:8080/api/posts/get');
+    return this.http.get(
+      'http://127.0.0.1:8080/api/v1/messages/all',
+      this.getHeaders()
+    );
   }
 
   insertPost(message: string) {
